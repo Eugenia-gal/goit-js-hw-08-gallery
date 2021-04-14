20b5212e34599556b4e238582a9bf3943ef2981d
Eugenia-gal <galevbor12@gmail.com>
2021-04-14 16:42:38 +0300

    adds event listeners

--- view-gallery.js
+++ view-gallery.js
@@ -1,13 +1,20 @@
 import galleryItems from './gallery-items.js';
 
 const galleryContainerEl = document.querySelector('.js-gallery');
-
-const galleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('\n');
-galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
-
+const lightBoxEl = document.querySelector('.js-lightbox');
+const lightBoxImageEl = lightBoxEl.querySelector('img');
 
 
+createGallery(galleryItems);
 
+galleryContainerEl.addEventListener('click', onGalleryItemClick);
+lightBoxEl.addEventListener('click', onModalCloseBtnClick);
+
+
+function createGallery(items) {
+    const galleryMarkup = items.map(makeGalleryItemMarkup).join('\n');
+    galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
+}
 
 function  makeGalleryItemMarkup({ preview, original, description }) {
     return ` <li class="gallery__item">
@@ -25,4 +32,23 @@ function  makeGalleryItemMarkup({ preview, original, description }) {
 </li>`
 }
 
+
+function onGalleryItemClick(e) {
+    e.preventDefault();
+
+    lightBoxImageEl.src = e.target.dataset.source;
+    lightBoxImageEl.alt = e.target.alt;
+
+    lightBoxEl.classList.add('is-open');
+}
+
+function onModalCloseBtnClick(e) {
+    if (e.target.nodeName !== "BUTTON") {
+        return;
+    }
+    
+    lightBoxImageEl.src = "";
+    lightBoxImageEl.alt = "";
+    lightBoxEl.classList.remove('is-open');
+}
 
